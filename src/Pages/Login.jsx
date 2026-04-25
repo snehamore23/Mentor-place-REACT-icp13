cdd8a92861cab6e1c478407007a634382ff39ebc
vaibhavwarade815 <varadevaibhav815@gmail.com>
2026-04-25 17:17:34 +0530

    validation added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -15,3 +15,25 @@ export default function Login() {
     const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
   };
+
+  const validate = () => {
+    let newErrors = {};
+
+    if (isSignup && !form.name.trim()) {
+      newErrors.name = "Name is required";
+    }
+
+    if (!form.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!form.email.includes("@")) {
+      newErrors.email = "Enter valid email";
+    }
+
+    if (!form.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (form.password.length < 6) {
+      newErrors.password = "Min 6 characters required";
+    }
+
+    return newErrors;
+  };
