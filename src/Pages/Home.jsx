15b3e96fa16392d672eb8a8deaba89f9d5501570
vaibhavwarade815 <varadevaibhav815@gmail.com>
2026-04-25 16:47:06 +0530

    feature-card added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,12 @@
 import "../style/Home.css";
 import { useNavigate } from "react-router-dom";
 
+function FeatureCard({ title, desc, icon }) {
+  return (
+    <div className="card">
+      <div className="icon">{icon}</div>
+      <h3>{title}</h3>
+      <p>{desc}</p>
+    </div>
+  );
+}
