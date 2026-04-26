784927a7d099cc439ff85e5f103bb6ecb9c17ef5
rinalshinde10 <rnlshnd@gmail.com>
2026-04-26 23:54:23 +0530

    all card create

--- src/views/PlacementPreparation/PlacementPreparation.jsx
+++ src/views/PlacementPreparation/PlacementPreparation.jsx
@@ -10,6 +10,48 @@ function PlacementPreparation() {
       desc: "Data Structures & Algorithms (Core for coding)",
       path: "/dsa",
     },
+   {
+      title: "Aptitude",
+      desc: "Quantitative aptitude for first round",
+      path: "/aptitude",
+    },
+    {
+      title: "Logical Reasoning",
+      desc: "Puzzles & problem solving skills",
+      path: "/logical",
+    },
+    {
+      title: "Verbal Ability",
+      desc: "Grammar & communication skills",
+      path: "/verbal",
+    },
+
+    
+    {
+      title: "C Programming",
+      desc: "Basics of C language and logic building",
+      path: "/c",
+    },
+    {
+      title: "C++",
+      desc: "Object-oriented programming concepts",
+      path: "/cpp",
+    },
+    {
+      title: "Java",
+      desc: "Core Java and OOP concepts",
+      path: "/java",
+    },
+    {
+      title: "Python",
+      desc: "Easy syntax and scripting language",
+      path: "/python",
+    },
+    {
+      title: "SQL",
+      desc: "Database queries and data management",
+      path: "/sql",
+    },
  ];
 
  return (
