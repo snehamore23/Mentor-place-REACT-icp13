cf69b4185a8f7e3b78557bead891cefa5ef7e0ed
rinalshinde10 <rnlshnd@gmail.com>
2026-04-26 23:49:17 +0530

    add logic

--- src/views/PlacementPreparation/PlacementPreparation.jsx
+++ src/views/PlacementPreparation/PlacementPreparation.jsx
@@ -3,4 +3,27 @@ import "./PlacementPreparation.css";
 
 function PlacementPreparation() {
   const navigate = useNavigate();
+
+ const cards = [
+
+ ];
+
+ return (
+    <div className="container">
+      <h1>Placement Preparation</h1>
+
+      <div className="card-container">
+        {cards.map((card, index) => (
+          <div
+            key={index}
+            className="card"
+            onClick={() => navigate(card.path)}
+          >
+            <h2>{card.title}</h2>
+            <p>{card.desc}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
