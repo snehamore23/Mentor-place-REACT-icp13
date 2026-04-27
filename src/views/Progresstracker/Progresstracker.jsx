7cafe427a9113d95b1afb12926e31cb69db4af08
snehamore23 <moresneha806@gmail.com>
2026-04-27 23:03:47 +0530

    done

--- src/views/Progresstracker/Progresstracker.jsx
+++ src/views/Progresstracker/Progresstracker.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import "./Progresstracker.css";
