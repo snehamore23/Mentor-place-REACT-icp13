c370c33f3f1485d1b6af81f8e5803cc7c5d63e2d
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 09:50:18 +0530

    logic add

--- src/views/Logical/Logical.jsx
+++ src/views/Logical/Logical.jsx
@@ -1,9 +1,20 @@
 import "./Logical.css";
 
 function Logical() {
-  const topics = [];
-  return(
-    <div></div>
-  );
+    const topics = [];
+    return (
+        <div className="container">
+            <h1>Logical Reasoning</h1>
+
+            {topics.map((t, i) => (
+                <div key={i} className="card">
+                    <h3>{t.name}</h3>
+                    <p>{t.note}</p>
+                    <input type="checkbox" onChange={() => toggle(item.name)} />
+                    <span> Completed</span>
+                </div>
+            ))}
+        </div>
+    );
 }
 export default Logical;
