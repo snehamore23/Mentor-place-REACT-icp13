07fd9afb6bfd7db3d722a05d8bf0260223b0edf5
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 09:55:45 +0530

    logic add in verbal

--- src/views/Verbal/Verbal.jsx
+++ src/views/Verbal/Verbal.jsx
@@ -3,8 +3,19 @@ import "./Verbal.css";
 function Verbal() {
   const topics = [];
 
-  return( <div>
+  return( 
+ <div className="container">
+      <h1>Verbal Ability</h1>
 
-  </div>);
+      {topics.map((t, i) => (
+        <div key={i} className="card">
+          <h3>{t.name}</h3>
+          <p>{t.note}</p>
+          <input type="checkbox" onChange={() => toggle(item.name)} />
+          <span> Completed</span>
+        </div>
+      ))}
+    </div>
+  );
 }
 export default Verbal;
