decd54923c537c96484ad6739c35db96e821c8d4
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 10:25:49 +0530

    add logic in this

--- src/views/Python/Python.jsx
+++ src/views/Python/Python.jsx
@@ -3,8 +3,25 @@ import "./Python.css";
 
 function Python() {
   const topics = [];
-  return(
-    <div></div>
+  const [checked, setChecked] = useState({});
+
+  const toggle = (name) => {
+    setChecked({ ...checked, [name]: !checked[name] });
+  };
+
+  return (
+    <div className="container">
+      <h1>Python Preparation</h1>
+
+      {topics.map((item, index) => (
+        <div key={index} className="card">
+          <h3>{item.name} ({item.level})</h3>
+          <p>{item.note}</p>
+          <input type="checkbox" onChange={() => toggle(item.name)} />
+          <span> Completed</span>
+        </div>
+      ))}
+    </div>
   );
 }
 
