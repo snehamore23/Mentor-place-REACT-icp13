b553439298ade0ac205d5e6e51c07a0ad2f8e47e
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 10:29:11 +0530

    add logic

--- src/views/SQL/SQL.jsx
+++ src/views/SQL/SQL.jsx
@@ -4,8 +4,25 @@ import "./SQL.css";
 function SQL() {
   const topics = [];
 
-  return(
-    <div></div>
+  const [checked, setChecked] = useState({});
+
+  const toggle = (name) => {
+    setChecked({ ...checked, [name]: !checked[name] });
+  };
+
+  return (
+    <div className="container">
+      <h1>SQL Preparation</h1>
+
+      {topics.map((item, index) => (
+        <div key={index} className="card">
+          <h3>{item.name} ({item.level})</h3>
+          <p>{item.note}</p>
+          <input type="checkbox" onChange={() => toggle(item.name)} />
+          <span> Completed</span>
+        </div>
+      ))}
+    </div>
   );
 }
 
