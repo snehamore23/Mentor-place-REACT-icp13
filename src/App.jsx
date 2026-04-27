7153c9f462a4d98f4b7b7a4c06e5a9cb45b2d416
snehamore23 <moresneha806@gmail.com>
2026-04-27 23:13:28 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { ThemeProvider } from "./context/ThemeContext";
 
