a34705558590ea514013f7e5c6676a4665709760
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 10:00:06 +0530

    add logic in c page

--- src/views/C/C.jsx
+++ src/views/C/C.jsx
@@ -4,7 +4,18 @@ import "./C.css";
 function C() {
   const topics = [];
   return(
-    <div></div>
+    <div className="container">
+      <h1>C Programming</h1>
+
+      {topics.map((item, index) => (
+        <div key={index} className="card">
+          <h3>{item.name} ({item.level})</h3>
+          <p>{item.note}</p>
+          <input type="checkbox" onChange={() => toggle(item.name)} />
+          <span> Completed</span>
+        </div>
+      ))}
+    </div>
   );
 }
 export default C;
