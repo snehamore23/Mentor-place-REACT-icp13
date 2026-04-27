97c246301a68ca75665bf7bf70c972d13f57cf0b
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 10:21:19 +0530

    add logic in java page

--- src/views/Java/Java.jsx
+++ src/views/Java/Java.jsx
@@ -4,8 +4,25 @@ import "./Java.css";
 function Java() {
   const topics = [];
 
-  return(
-    <div></div>
+ const [checked, setChecked] = useState({});
+
+  const toggle = (name) => {
+    setChecked({ ...checked, [name]: !checked[name] });
+  };
+
+  return (
+    <div className="container">
+      <h1>Java Preparation</h1>
+
+      {topics.map((item, index) => (
+        <div key={index} className="card">
+          <h3>{item.name} ({item.level})</h3>
+          <p>{item.note}</p>
+          <input type="checkbox" onChange={() => toggle(item.name)} />
+          <span> Completed</span>
+        </div>
+      ))}
+    </div>
   );
 }
 export default Java;
