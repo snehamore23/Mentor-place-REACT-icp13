16e9dbffa402baa9a5a900d8508429001615de0a
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 10:10:37 +0530

    add css in cpp

--- src/views/Cpp/Cpp.jsx
+++ src/views/Cpp/Cpp.jsx
@@ -3,13 +3,20 @@ import "./Cpp.css";
 
 function Cpp() {
   const topics = [
-    
+
+  {
+      name: "Inheritance",
+      level: "Medium",
+      note: "Inheritance allows code reuse. Child class inherits parent. Improves design. Important OOP concept. Common in interviews.",
+    },
   ];
- const [checked, setChecked] = useState({});
+
+  const [checked, setChecked] = useState({});
 
   const toggle = (name) => {
     setChecked({ ...checked, [name]: !checked[name] });
   };
+
   return(
    <div className="container">
       <h1>C++ Preparation</h1>
