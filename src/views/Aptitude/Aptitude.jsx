9172c32699cf353b38c784d3d75cd3d8a9af0215
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 09:43:55 +0530

    logic add in aptitude

--- src/views/Aptitude/Aptitude.jsx
+++ src/views/Aptitude/Aptitude.jsx
@@ -4,8 +4,31 @@ import "./Aptitude.css";
 function Aptitude() {
   const topics = [];
 
-  return(
-    <div></div>
+  const [done, setDone] = useState([]);
+
+  const toggle = (topic) => {
+    setDone((prev) =>
+      prev.includes(topic)
+        ? prev.filter((t) => t !== topic)
+        : [...prev, topic]
+    );
+  };
+
+  return (
+    <div className="container">
+      <h1>Aptitude Preparation</h1>
+
+      {topics.map((t, i) => (
+        <div key={i} className="card">
+          <h3>{t.name}</h3>
+          <p>{t.note}</p>
+          <input type="checkbox" onChange={() => toggle(t.name)} />
+          <span> Completed</span>
+        </div>
+      ))}
+
+     
+    </div>
   );
 }
 export default Aptitude;
