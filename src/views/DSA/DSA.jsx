fbbc967563df504fc8c55eae26e6d31ca774be98
rinalshinde10 <rnlshnd@gmail.com>
2026-04-27 09:35:49 +0530

    Add logic

--- src/views/DSA/DSA.jsx
+++ src/views/DSA/DSA.jsx
@@ -3,5 +3,13 @@ import "./DSA.css";
 
 function DSA() {
   const topics = [];
+  const [checked, setChecked] = useState({});
+
+  const toggle = (name) => {
+    setChecked({ ...checked, [name]: !checked[name] });
+  };
+  return(
+    <div><h1>DSA Preparation</h1></div>
+  )
 }
 export default DSA;
